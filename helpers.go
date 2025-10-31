abb3fe2fa5a2385d85186ae5d83e4b450309af02
Giira <euan.a.marsh@gmail.com>
2025-10-31 23:37:04 +0000

    return error json middleware

--- helpers.go
+++ helpers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func (cfg *apiConfig) metricInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +18,12 @@ func returnJSON(w http.ResponseWriter, code int, payload interface{}) {
 
 func returnError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
-
+		log.Printf("5xx error: %s", msg)
 	}
+	type errorReturn struct {
+		Error string `json"error"`
+	}
+	returnJSON(w, code, errorReturn{
+		Error: msg,
+	})
 }
