9e2cde8f01a8c4947cd2f7de56d014e23526d229
Giira <euan.a.marsh@gmail.com>
2025-11-12 21:06:37 +0000

    auto load environment and database

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
 
+	"github.com/Giira/chirpy/internal/database"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	queries        *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("error launching database: %w", err)
+	}
+	dbQueries := database.New(db)
 
 	port := "8080"
 	root := "."
-	cfg := &apiConfig{}
+	cfg := &apiConfig{
+		queries: dbQueries,
+	}
 
 	serveMux := http.NewServeMux()
 	file_handler := http.StripPrefix("/app", http.FileServer(http.Dir(root)))
